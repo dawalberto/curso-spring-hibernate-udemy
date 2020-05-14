80c02eaee5fcc162fc21648a7e0b04befd190477
Alberto Garcia <albertoinformatica97@gmail.com>
2020-05-14 19:00:31 +0200

    Activity 1 completed

---
+++ spring-demo-one/src/com/dawalberto/springdemo/UnicycleCoach.java
@@ -0,0 +1,10 @@
+package com.dawalberto.springdemo;
+
+public class UnicycleCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Do 5 rolling wraps and 1 360";
+	}
+
+}
