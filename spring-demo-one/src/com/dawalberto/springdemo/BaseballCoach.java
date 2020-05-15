9a0d641f9adc58842d7c4d83ac47c5782958eb41
Alberto Garcia <albertoinformatica97@gmail.com>
2020-05-15 11:10:01 +0200

    Development Process - Constructor Injection. 2 - Create a constructor in your class for injections ✅

--- spring-demo-one/src/com/dawalberto/springdemo/BaseballCoach.java
+++ spring-demo-one/src/com/dawalberto/springdemo/BaseballCoach.java
@@ -2,6 +2,14 @@ package com.dawalberto.springdemo;
 
 public class BaseballCoach implements Coach {
 
+	// define a private field for the dependency
+	private FortuneService fortuneService;
+	
+	// define a constructor for the dependency injeciton
+	public BaseballCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting practice";
@@ -9,8 +17,8 @@ public class BaseballCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		// use my fortuneService to get a fortune
+		return fortuneService.getFortune();
 	}
 	
 }
