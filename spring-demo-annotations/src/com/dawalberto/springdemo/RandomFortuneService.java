b3b66d855715b4c1b49c5c7b99e38d8f5bfb3ae6
Alberto Garcia <albertoinformatica97@gmail.com>
2020-05-20 11:31:11 +0200

    RandomFortuneService

--- spring-demo-annotations/src/com/dawalberto/springdemo/RandomFortuneService.java
+++ spring-demo-annotations/src/com/dawalberto/springdemo/RandomFortuneService.java
@@ -1,14 +1,22 @@
 package com.dawalberto.springdemo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneService {
 
+	
+	private String[] fortunes = {
+			"Today is your lucky day!", 
+			"Every man is the architect of his own fortune.", 
+			"Fortune befriends the bold."
+	};
+	
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return fortunes[new Random().nextInt(fortunes.length)];
 	}
 
 }
