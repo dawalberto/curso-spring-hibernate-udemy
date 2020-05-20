5c7e029355754eb5a84f9b9b6c296764a668b681
Alberto Garcia <albertoinformatica97@gmail.com>
2020-05-20 11:52:03 +0200

    Qualifier Constructor

--- spring-demo-annotations/src/com/dawalberto/springdemo/UnicycleCoach.java
+++ spring-demo-annotations/src/com/dawalberto/springdemo/UnicycleCoach.java
@@ -1,20 +1,18 @@
 package com.dawalberto.springdemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UnicycleCoach implements Coach {
 
-	@Qualifier("happyFortuneService")
-	@Autowired
 	private FortuneService fortuneService;
 	
 	// As a Spring Framework 4.3 an @Autowired annotaion is no longer necessary if the bean only defines one constructor.
-//	public UnicycleCoach(FortuneService theFortuneService) {
-//		fortuneService = theFortuneService;
-//	}
+	// If @Qualifier is in Constructor the syntax is much different than in Field
+	public UnicycleCoach(@Qualifier("happyFortuneService")FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
 	
 	@Override
 	public String getDailyWorkout() {
