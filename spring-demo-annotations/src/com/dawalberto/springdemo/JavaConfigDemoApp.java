3143e6a1a955876829656e0bb377faafebb3feb7
Alberto Garcia <albertoinformatica97@gmail.com>
2020-05-22 14:18:06 +0200

    Spring Config with Java Class (no XML)

---
+++ spring-demo-annotations/src/com/dawalberto/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,21 @@
+package com.dawalberto.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+		// read spring config java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+		
+		// get the bean from spring container
+		Coach coach = context.getBean("tennisCoach", Coach.class);
+		
+		// call a method on the bean
+		System.out.println(coach.getDailyWorkout());
+		
+		// close context
+		context.close();
+	}
+	
+}
---
+++ spring-demo-annotations/src/com/dawalberto/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.dawalberto.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.dawalberto.springdemo")
+public class SportConfig {
+
+}
