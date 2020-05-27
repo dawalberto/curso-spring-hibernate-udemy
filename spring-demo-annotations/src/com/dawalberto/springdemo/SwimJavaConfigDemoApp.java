83b77f903090dbab9dfe43e1f3dee543620fcc3f
Alberto Garcia <albertoinformatica97@gmail.com>
2020-05-27 15:30:28 +0200

    Field Injection from .properties file with Annotations

--- spring-demo-annotations/src/com/dawalberto/springdemo/SwimJavaConfigDemoApp.java
+++ spring-demo-annotations/src/com/dawalberto/springdemo/SwimJavaConfigDemoApp.java
@@ -9,11 +9,17 @@ public class SwimJavaConfigDemoApp {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 		
 		// get the bean from spring container
-		Coach coach = context.getBean("swimCoach", Coach.class);
+		SwimCoach coach = context.getBean("swimCoach", SwimCoach.class);
 		
 		// call a method on the bean
 		System.out.println(coach.getDailyWorkout());
+		
+		// call method to get the daily fortune
 		System.out.println(coach.getDailyFortune());
+		
+		// call our new swim coach methods ... has the props values injected
+		System.out.println("email: " + coach.getEmail());
+		System.out.println("team: " + coach.getTeam());
 		
 		// close context
 		context.close();
