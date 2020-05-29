f3a3c740ec94fb9c8614155f8e6dce246a225a43
Alberto Garcia <albertoinformatica97@gmail.com>
2020-05-29 12:54:50 +0200

    int --> Integer to correct validation

--- spring-mvc-demo/src/com/dawalberto/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/dawalberto/springdemo/mvc/Customer.java
@@ -17,7 +17,7 @@ public class Customer {
 	@NotNull(message = "is required")
 	@Min(value = 0, message = "must be greater or equals than 0")
 	@Max(value = 10, message = "must be less or equals than 10")
-	private int freePasses;
+	private Integer freePasses;
 	
 	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only letters and numbers and 5 length is allowed")
 	private String postalCode;
@@ -38,11 +38,11 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(int freepasses) {
+	public void setFreePasses(Integer freepasses) {
 		this.freePasses = freepasses;
 	}
 
