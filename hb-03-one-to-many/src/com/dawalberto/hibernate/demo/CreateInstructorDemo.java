0824b5b2067d8f1947c6c92c7d76546c5ed40e5a
Alberto Garcia <albertoinformatica97@gmail.com>
2020-10-10 12:32:50 +0200

    Hibernate One-to-Many Bi-Directional finished ✅

---
+++ hb-03-one-to-many/src/com/dawalberto/hibernate/demo/CreateInstructorDemo.java
@@ -0,0 +1,58 @@
+package com.dawalberto.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.dawalberto.hibernate.demo.entity.Course;
+import com.dawalberto.hibernate.demo.entity.Instructor;
+import com.dawalberto.hibernate.demo.entity.InstructorDetail;
+
+public class CreateInstructorDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+
+			// create the objects
+			Instructor tempInstructor = new Instructor("Luisina", "García", "luisina@luisinaudemy.com");			
+			InstructorDetail tempInstructorDetail = new InstructorDetail("http://luisinayoutubechannel.com", "dance");
+			
+			// associate the objects
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the instructor
+			//
+			// Note: this will ALSO save the details object
+			// because of CascadeType.ALL
+			//
+			System.out.println("Saving instructor: " + tempInstructor);
+			session.save(tempInstructor);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			session.close();
+			factory.close();
+		}
+		
+	}
+
+}
---
+++ hb-03-one-to-many/src/com/dawalberto/hibernate/demo/GetInstructorDetailDemo.java
@@ -0,0 +1,55 @@
+package com.dawalberto.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.dawalberto.hibernate.demo.entity.Instructor;
+import com.dawalberto.hibernate.demo.entity.InstructorDetail;
+
+public class GetInstructorDetailDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+			
+			// get the instructor detail object
+			int id = 9;
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, id);
+			
+			// print the instructor detail
+			System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+			
+			// print the associated instructor
+			System.out.println("The associated instructor: " + tempInstructorDetail.getInstructor());
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			// handle connection leak issue
+			session.close();
+			factory.close();
+		}
+		
+	}
+
+}
